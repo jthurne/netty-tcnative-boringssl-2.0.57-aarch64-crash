3640fa7ad54611e004ed8812d4701d7e9078501f
Jim Hurne <jhurne@gradle.com>
2023-06-30 15:55:34 -0400

    Remove noise

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,24 +2,19 @@ import com.bmuschko.gradle.docker.tasks.container.*
 import com.bmuschko.gradle.docker.tasks.image.*
 
 plugins {
-    // Apply the application plugin to add support for building a CLI application in Java.
     application
     id("com.bmuschko.docker-java-application") version "9.3.1"
 }
 
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
 dependencies {
-    // This dependency is used by the application.
-    implementation("com.google.guava:guava:31.1-jre")
     implementation("io.netty:netty-all:4.1.94.Final")
     runtimeOnly("io.netty:netty-tcnative-boringssl-static:2.0.61.Final:linux-aarch_64")
 }
 
-// Apply a specific Java toolchain to ease working on different environments.
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
